db9ddb203ef6f4683506cbbf3b22c431bb3b7a5c
isaaregina <isabellaregina344@gmail.com>
2023-03-02 16:36:08 -0300

    feat: Criado método toString e SimpleDateFormat na classe Reservation

--- excecoes1/src/model/entities/Reservation.java
+++ excecoes1/src/model/entities/Reservation.java
@@ -1,5 +1,6 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -8,6 +9,8 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
 		this.roomNumber = roomNumber;
@@ -39,6 +42,19 @@ public class Reservation {
 	public void updateDates(Date checkIn, Date checkOut) {
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room "
+				+ roomNumber 
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out: "
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
 	}
 	
 	
