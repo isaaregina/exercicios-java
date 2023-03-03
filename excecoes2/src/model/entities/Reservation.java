60bbff548419f4be8346a7cf63367c478296e91e
isaaregina <isabellaregina344@gmail.com>
2023-03-02 21:19:24 -0300

    feat: Adicionado construtor e get set

--- excecoes2/src/model/entities/Reservation.java
+++ excecoes2/src/model/entities/Reservation.java
@@ -7,4 +7,26 @@ public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
+	
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkin;
+		this.checkOut = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
 }
