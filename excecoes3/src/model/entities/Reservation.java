cc190f31078a218e856eecff2c771cbbf1f7e713
isaaregina <isabellaregina344@gmail.com>
2023-03-25 19:56:18 -0300

    feat: Adicionado construtor e get set

--- excecoes3/src/model/entities/Reservation.java
+++ excecoes3/src/model/entities/Reservation.java
@@ -2,9 +2,42 @@ package model.entities;
 
 import java.util.Date;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
+	
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
+		
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+
+	
+	
 }
